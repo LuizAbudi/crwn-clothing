243fd06530ab54ad51e23175a04a5a2b32c73132
LuizAbudi <luizabudi@hotmail.com>
2024-03-08 15:27:42 -0300

    feat: adiciona pacote react-router-dom e atualiza dependências

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "Bonés",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "Jaquetas",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "Tênis",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "Mulheres",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "Homens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
   return (
     <div>
-      <Directory categories={categories} />
+      <Home />
     </div>
   );
 }
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import Directory from '../../components/directory/directory.component';
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Bonés',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jaquetas',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Tênis',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'Mulheres',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Homens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+  return (
+    <div>
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default Home;
