007b3313d439e1bd6fedde2ee5338f842bd857b7
LuizAbudi <luizabudi@hotmail.com>
2024-03-11 14:07:29 -0300

    feat: adiciona funcionalidade de login com Google Popup e criação de documento de usuário

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM6gy0lw4tShmzVzfeUkCsikVNZ798m5A",
+  authDomain: "crwn-colthing-db-2f1dd.firebaseapp.com",
+  projectId: "crwn-colthing-db-2f1dd",
+  storageBucket: "crwn-colthing-db-2f1dd.appspot.com",
+  messagingSenderId: "476792838279",
+  appId: "1:476792838279:web:f45ad751b09feaecbbc022"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.error('Error creating user', error.message);
+    }
+  }
+
+  return userDocRef;
+
+}
