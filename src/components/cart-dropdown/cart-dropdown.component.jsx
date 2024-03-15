5b81c97378385ed08aeabc930528c4f0d3d50d25
LuizAbudi <luizabudi@hotmail.com>
2024-03-15 15:27:44 -0300

    feat: atualizações de preços e estilos de visualização

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,17 @@
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
-import CartItem from '../cart-item/card-item.component';
-import { useContext } from 'react';
+import CartItem from '../cart-item/cart-item.component';
+import { useCallback, useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import { useNavigate } from 'react-router-dom';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const handleCheckout = useCallback(() => {
+    navigate('/checkout');
+  }, [navigate]);
 
   return (
     <div className="cart-dropdown-container">
@@ -15,10 +21,10 @@ const CartDropdown = () => {
             <CartItem key={cartItem.id} cartItem={cartItem} />
           ))
         ) : (
-          <span className="empty-message">Your cart is empty</span>
+          <span className="empty-message">Carrinho vazio</span>
         )}
       </div>
-      <Button>Finalizar compra</Button>
+      <Button onClick={handleCheckout}>Finalizar compra</Button>
     </div>
   );
 };
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
 
-import { CartContext } from '../../contexts/cart.context';
 import { Icon } from '@iconify/react';
 
 import './cart-icon.styles.scss';
+import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cardCount } = useContext(CartContext);
+  const { cartCount, isCartOpen, setIsCartOpen } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
@@ -14,7 +14,7 @@ const CartIcon = () => {
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <Icon icon="mdi-light:cart" className="shopping-icon" />
       <span className="item-count">
-        <span className="count">{cardCount}</span>
+        <span className="count">{cartCount}</span>
       </span>
     </div>
   );
