cbe577607c88b003525fc563e63c665eb733f165
LuizAbudi <luizabudi@hotmail.com>
2024-03-28 09:56:06 -0300

    feat: atualizar visualização de categoria com botão "Veja mais"

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import ProductCard from '../product-card/product-card.component';
 import './category-preview.styles.scss';
+import { Icon } from '@iconify/react';
 
 const CategoryPreview = ({ title, products }) => {
   return (
@@ -10,10 +11,16 @@ const CategoryPreview = ({ title, products }) => {
       </h2>
       <div className="preview">
         {products
-          .filter((product, idx) => idx < 4)
+          .filter((product, idx) => idx < 3)
           .map(({ id, ...otherProductProps }) => (
             <ProductCard key={id} product={otherProductProps} />
           ))}
+        <div className="see-more-container">
+          <Link className="see-more" to={`/shop/${title.toLowerCase()}`}>
+            <Icon icon="bi:arrow-right-square-fill" width={50} />
+            <span>Veja mais</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
